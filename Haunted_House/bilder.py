98f03d3059942845b16f55efc88ea0b5528e53bc
MarcusBjorgan <mtbjorgan@gmail.com>
2025-02-23 14:17:06 +0100

    Update bilder.py
    Endrer slik at filen ku inneholder informasjonen om bildet

--- Haunted_House/bilder.py
+++ Haunted_House/bilder.py
@@ -1,6 +1,5 @@
 import pygame
 from constants import*
 
-bg_image = pygame.image.load("Assets/bg.png").convert_alpha()
+bg_image = pygame.image.load("Haunted_House/Assets/bg.png").convert_alpha()
 
-bg_image = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
