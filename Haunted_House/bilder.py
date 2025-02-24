d3603f23e92e20754b98cefd3e2cc5367435c954
MarcusBjorgan <mtbjorgan@gmail.com>
2025-02-24 22:28:11 +0100

    Update bilder.py
    - Lagt til et bakgrunnsbilde for når man taper

--- Haunted_House/bilder.py
+++ Haunted_House/bilder.py
@@ -3,3 +3,7 @@ from constants import*
 
 bg_image = pygame.image.load("Haunted_House/Assets/bg.png").convert_alpha()
 
+wall_texture = pygame.image.load("Haunted_House/Assets/parquet.png").convert_alpha()
+
+wall_texture = pygame.transform.scale(wall_texture, (CELL_SIZE, CELL_SIZE))
+
